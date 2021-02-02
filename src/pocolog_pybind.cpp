1c0028e45ed795e7048f167083258202755bf5d4
a b <a@b.c>
2021-02-02 17:10:45 +0000

    Continue working on implementing python bindings for streams

--- src/pocolog_pybind.cpp
+++ src/pocolog_pybind.cpp
@@ -10,27 +10,54 @@
 
 namespace py = pybind11;
 
+
+void addValidInputDataStreams(
+    const std::vector<pocolog_cpp::Stream*>& streams,
+    std::vector<pocolog_cpp::InputDataStream*>& dataStreams)
+{
+    dataStreams.reserve(streams.size());
+    for(size_t i = 0; i < streams.size(); i++)
+    {
+        pocolog_cpp::InputDataStream* dataStream =
+            dynamic_cast<pocolog_cpp::InputDataStream*>(streams[i]);
+        if(!dataStream)
+        {
+            std::cerr << "[pocolog_pybind] Stream #" << i
+                      << " is not an InputDataStream, will be ignored!" << std::endl;
+            continue;
+        }
+        dataStreams.push_back(dataStream);
+    }
+}
+
 class PocologIterator {
 protected:
     int idx = 0;
 
     std::vector<std::string> logfiles;
     pocolog_cpp::MultiFileIndex multiIndex;
+    std::vector<pocolog_cpp::InputDataStream*> dataStreams;
 public:
     PocologIterator (const std::vector<std::string> logfiles_) {
         logfiles = logfiles_;
+
         pocolog_cpp::MultiFileIndex multiIndex = pocolog_cpp::MultiFileIndex();
         multiIndex.createIndex(logfiles);
         std::vector<pocolog_cpp::Stream*> streams = multiIndex.getAllStreams();
-        // std::vector<pocolog_cpp::InputDataStream*> dataStreams;
-        // addValidInputDataStreams(streams, dataStreams, only);
-        std::cout << "[pocolog_pybind] " << streams.size() << " streams" << std::endl;
+        addValidInputDataStreams(streams, dataStreams);
+        std::cout << "[pocolog_pybind] " << dataStreams.size() << " streams" << std::endl;
+    }
+    pocolog_cpp::MultiFileIndex getMultiFileIndex(){
+        return multiIndex;
     }
     int getIndex() {
         return idx;
     }
     void __next__() {
         /* we iterate to next */
+    }
+    std::vector<pocolog_cpp::InputDataStream*> getInputDataStreams() {
+        return dataStreams;
     }
 };
 
@@ -44,10 +71,39 @@ PYBIND11_MODULE(pocolog_pybind, m) {
            __init__
     )pbdoc";
 
+    py::class_<pocolog_cpp::Stream>(m, "Stream")
+        .def("get_name", &pocolog_cpp::InputDataStream::getName)
+        .def("get_type_name", &pocolog_cpp::InputDataStream::getTypeName)
+        // .def("get_stream_type", &pocolog_cpp::InputDataStream::getStreamType)
+        .def("get_first_sample_time", &pocolog_cpp::InputDataStream::getFistSampleTime)
+        .def("get_last_sample_time", &pocolog_cpp::InputDataStream::getLastSampleTime)
+        .def("get_index", &pocolog_cpp::InputDataStream::getIndex)
+        .def("get_size", &pocolog_cpp::InputDataStream::getSize)
+        .def("get_sample_data", &pocolog_cpp::InputDataStream::getSampleData)
+    ;
+
+    py::class_<pocolog_cpp::InputDataStream>(m, "InputDataStream")
+        .def("get_cxx_type", &pocolog_cpp::InputDataStream::getCXXType)
+        .def("get_type_memory_size", &pocolog_cpp::InputDataStream::getTypeMemorySize)
+        .def("get_sample_data", &pocolog_cpp::InputDataStream::getSampleData)
+    ;
+
+    py::class_<pocolog_cpp::MultiFileIndex>(m, "MultiFileIndex")
+        .def("get_size", &pocolog_cpp::MultiFileIndex::getSize)
+        //.def("get_global_stream_idx", &pocolog_cpp::MultiFileIndex::getGlobalStreamIdx)
+        .def("get_pos_in_stream", &pocolog_cpp::MultiFileIndex::getPosInStream)
+        .def("get_sample_stream", &pocolog_cpp::MultiFileIndex::getSampleStream)
+        // bool createIndex(const std::vector<std::string> &fileNames);
+        // .def("create_index", static_cast<bool (MultiFileIndex::*)(std::vector< std::string >&)>(&pocolog_cpp::MultiFileIndex::createIndex), "create a pocolog index")
+        // .def("get_global_stream_idx", &pocolog_cpp::MultiFileIndex::getGlobalStreamIdx)
+    ;
+
     
     py::class_<PocologIterator>(m, "PocologIterator")
         .def(py::init<const std::vector<std::string>>())
         .def("get_index", &PocologIterator::getIndex)
+        .def("get_multi_file_index", &PocologIterator::getMultiFileIndex)
+        .def("get_input_data_streams", &PocologIterator::getInputDataStreams)
         /*.def("__len__", [](const std::vector<int> &v) { return v.size(); })
         .def("__iter__", [](std::vector<int> &v) {
            return py::make_iterator(v.begin(), v.end());
